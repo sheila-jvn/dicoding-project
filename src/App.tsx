9831c07bf012bd10647e16e9527f553741bf0408
Sheila Putri Javana <jvnsheila@gmail.com>
2024-06-18 18:48:30 +0700

    feat: adjusting design home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { UploadCloud } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col gap-4 items-center">
+        <div className="gap-2 flex flex-col border border-[#3f3f46] rounded-[10px] p-6 w-[450px] items-center">
+          <Button className="w-[208px]">
+            Sync w/ Google Drive <UploadCloud className="ml-2 h-4 w-4" />
+          </Button>
+          <p className="leading-7 [&:not(:first-child)]:mt-2">
+            For security, this app only accesses files it creates.
+          </p>
+        </div>
+
+        <div className="gap-4 flex flex-col border border-[#3f3f46] rounded-[10px] p-6 w-[450px] justify-center items-center">
+          <Button className="w-full">Add 2FA</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
