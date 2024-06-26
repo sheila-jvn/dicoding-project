ca865046b9e15bc689ebbf7e0c126bc06387bd06
Erick Christian <erickchristian48@gmail.com>
2024-06-26 16:26:36 +0700

    config: setup eslint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
-import { UploadCloud } from "lucide-react";
+import "./App.css"
+import { Button } from "./components/ui/button"
+import { UploadCloud } from "lucide-react"
 
 function App() {
   return (
     <>
-      <div className="flex flex-col gap-4 items-center">
-        <div className="gap-2 flex flex-col border border-[#3f3f46] rounded-[10px] p-6 w-[450px] items-center">
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex w-[450px] flex-col items-center gap-2 rounded-[10px] border border-[#3f3f46] p-6">
           <Button className="w-[208px]">
             Sync w/ Google Drive <UploadCloud className="ml-2 h-4 w-4" />
           </Button>
@@ -15,12 +15,12 @@ function App() {
           </p>
         </div>
 
-        <div className="gap-4 flex flex-col border border-[#3f3f46] rounded-[10px] p-6 w-[450px] justify-center items-center">
+        <div className="flex w-[450px] flex-col items-center justify-center gap-4 rounded-[10px] border border-[#3f3f46] p-6">
           <Button className="w-full">Add 2FA</Button>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ import animated from "tailwindcss-animate"
 export default {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
